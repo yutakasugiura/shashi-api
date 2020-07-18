ea3d453ce20273309dfeab75590df9d189fb1f9f
Your Name <19901220bbb@gmail.com>
2020-07-18 15:49:35 +0900

    api連携の構築

---
+++ client/pages/api.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <p>{{ posts }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8000/api/company/")
+      .then(response => (this.posts = response));
+  }
+};
+</script>

592954accda56d78993a7dcb9cbe73613775ef1b
Your Name <19901220dev@gmail.com>
2020-07-20 23:39:06 +0900

    APIの層を分離

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Models\Company;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CompanyController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/company', 'CompanyController');
+Route::apiResource('/company', 'API\CompanyController');
